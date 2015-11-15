f23e85a0c7da31f4c6e57c4394f4236413bcb7ac
lants1 <swen.lanthemann@gmail.com>
2015-11-15 20:17:03 +0100

    QuotaUpdater implemented

--- fce/src/main/java/org/eclipse/moquette/fce/job/QuotaUpdater.java
+++ fce/src/main/java/org/eclipse/moquette/fce/job/QuotaUpdater.java
@@ -1,7 +1,17 @@
 package org.eclipse.moquette.fce.job;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Map.Entry;
 import java.util.logging.Logger;
 
+import org.eclipse.moquette.fce.model.quota.PeriodicQuotaState;
+import org.eclipse.moquette.fce.model.quota.Quota;
+import org.eclipse.moquette.fce.model.quota.QuotaState;
+import org.eclipse.moquette.fce.model.ManagedCycle;
 import org.eclipse.moquette.fce.service.FceServiceFactory;
 
 public class QuotaUpdater implements Runnable {
@@ -17,10 +27,46 @@ public class QuotaUpdater implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO lants1 reset quota counters according to cycle intervall
-		// is called every hour needs to check which intervall is elapsed
-
+		updateQuota();
 		log.info("JOB: completed succefully");
+	}
+
+	private void updateQuota() {
+
+		for (Entry<String, Quota> entry : services.getQuotaDbService().getAll()) {
+			String key = entry.getKey();
+			Quota quotaIn = entry.getValue();
+			List<QuotaState> statesOut = new ArrayList<>();
+
+			for (QuotaState state : quotaIn.getQuotaState()) {
+				if (state instanceof PeriodicQuotaState) {
+					PeriodicQuotaState newPeriodicState = (PeriodicQuotaState) state;
+					if (isCycleExpired(newPeriodicState.getCycle(), newPeriodicState.getLastManagedTimestamp())) {
+						newPeriodicState.setCurrentQuotaBytes(0);
+						newPeriodicState.setCurrentQuotaCount(0);
+						newPeriodicState.setLastManagedTimestamp(new Date());
+					}
+					statesOut.add(newPeriodicState);
+				} else {
+					statesOut.add(state);
+				}
+			}
+
+			Quota quotaOut = new Quota(quotaIn.getUsergroup(), statesOut);
+			services.getQuotaDbService().put(key, quotaOut);
+		}
+
+	}
+
+	private boolean isCycleExpired(ManagedCycle cycle, Date lastManagedTimestamp) {
+		Calendar now = Calendar.getInstance();
+		Calendar past = new GregorianCalendar();
+		past.setTime(lastManagedTimestamp);
+
+		if (now.get(cycle.getCalendarReference()) != past.get(cycle.getCalendarReference())) {
+			return true;
+		}
+		return false;
 	}
 
 }
