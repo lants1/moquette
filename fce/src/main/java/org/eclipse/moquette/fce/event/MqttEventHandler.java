a16cb68ce0e556931998fdb12110106f708f24ad
u214882 <swen.lanthemann@sbb.ch>
2015-11-19 12:02:57 +0100

    ManagedTopic Object instead of String as topicIdentifer

--- fce/src/main/java/org/eclipse/moquette/fce/event/MqttEventHandler.java
+++ fce/src/main/java/org/eclipse/moquette/fce/event/MqttEventHandler.java
@@ -2,10 +2,10 @@ package org.eclipse.moquette.fce.event;
 
 import java.util.logging.Logger;
 
-import org.eclipse.moquette.fce.common.ManagedTopicIdentifierUtil;
 import org.eclipse.moquette.fce.common.ManagedZone;
 import org.eclipse.moquette.fce.common.ManagedZoneUtil;
 import org.eclipse.moquette.fce.exception.FceSystemFailureException;
+import org.eclipse.moquette.fce.model.ManagedTopic;
 import org.eclipse.moquette.fce.model.configuration.UserConfiguration;
 import org.eclipse.moquette.fce.model.quota.Quota;
 import org.eclipse.moquette.fce.service.FceServiceFactory;
@@ -17,7 +17,7 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 public class MqttEventHandler implements MqttCallback {
 
 	private final static Logger log = Logger.getLogger(MqttEventHandler.class.getName());
-	
+
 	FceServiceFactory services;
 
 	public MqttEventHandler(FceServiceFactory services) {
@@ -43,27 +43,29 @@ public class MqttEventHandler implements MqttCallback {
 	public void messageArrived(String topicIdentifier, MqttMessage message) throws Exception {
 		ManagedZone topicZone = ManagedZoneUtil.getZoneForTopic(topicIdentifier);
 		String msgPayload = String.valueOf(message.getPayload());
-		String userTopic;
-		
+		ManagedTopic topic = new ManagedTopic(topicIdentifier);
+
 		switch (topicZone) {
 		case MANAGED_INTENT:
 		case MANAGED_CONFIGURATION:
 			// less safety, login and hash needs to be sent in the same message
 			// direct access to the broker would be a safer solution
-			// in this case there would be no need to transmit the hash and login information in the same message
-			// TODO Lan maybe implement another handler for internal broker actions
+			// in this case there would be no need to transmit the hash and
+			// login information in the same message
+			// TODO Lan maybe implement another handler for internal broker
+			// actions
 			UserConfiguration msgConfig = services.getJsonParser().deserializeUserConfiguration(msgPayload);
-			userTopic = ManagedTopicIdentifierUtil.addUserSpecificTopicFilter(topicIdentifier, msgConfig);
-			services.getConfigDbService().put(userTopic, msgConfig);
-			services.getMqttService().publish(userTopic, msgPayload, true);
+			services.getConfigDbService().put(topic.getUserTopicIdentifier(msgConfig, topicZone), msgConfig);
+			services.getMqttService().publish(topic.getUserTopicIdentifier(msgConfig, topicZone), msgPayload,
+					true);
 			log.fine("received configuration message for topic: " + topicIdentifier);
 			break;
 		case MANAGED_QUOTA:
 			// TODO Lan generic implementation....
 			Quota msgQuota = services.getJsonParser().deserializeQuota(msgPayload);
-			userTopic = ManagedTopicIdentifierUtil.addUserSpecificTopicFilter(topicIdentifier, msgQuota);
-			services.getQuotaDbService().put(topicIdentifier, msgQuota);
-			services.getMqttService().publish(userTopic, msgPayload, true);
+			services.getQuotaDbService().put(topic.getUserTopicIdentifier(msgQuota, topicZone), msgQuota);
+			services.getMqttService().publish(topic.getUserTopicIdentifier(msgQuota, topicZone), msgPayload,
+					true);
 			log.fine("received quota message for topic: " + topicIdentifier);
 			break;
 		default:
--- fce/src/test/java/org/eclipse/moquette/fce/service/ConfigurationDbServiceTest.java
+++ fce/src/test/java/org/eclipse/moquette/fce/service/ConfigurationDbServiceTest.java
@@ -4,8 +4,8 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.eclipse.moquette.fce.common.ManagedZone;
-import org.eclipse.moquette.fce.common.ManagedZoneUtil;
 import org.eclipse.moquette.fce.exception.FceNoAuthorizationPossibleException;
+import org.eclipse.moquette.fce.model.ManagedTopic;
 import org.eclipse.moquette.fce.model.configuration.ManagedPermission;
 import org.eclipse.moquette.fce.model.configuration.UserConfiguration;
 import org.eclipse.moquette.plugin.AuthorizationProperties;
@@ -15,28 +15,28 @@ public class ConfigurationDbServiceTest {
 
 	private static final String TESTIDENTIFIER = "testidentifier";
 	private static final String TESTUSER = "testuser";
-	private static final String TOPIC_SAMPLE = "/house/firstfloor/light";
-	private static final String TOPIC_SAMPLE_CONFIG_IN_SUBTOPIC = "/house/firstfloor/light/morelight";
-	private static final String TOPIC_INVALID = "/asdf/asdfasfd/dfasf";
+	
+	private static final String ALLIDENTIFIER = "allidentifier";
+	private static final String ALLUSER = "alluser";
+	
+	private static final ManagedTopic TOPIC_SAMPLE = new ManagedTopic("/house/firstfloor/light");
+	private static final ManagedTopic TOPIC_SAMPLE_CONFIG_IN_SUBTOPIC = new ManagedTopic("/house/firstfloor/light/morelight");
+	private static final ManagedTopic TOPIC_INVALID = new ManagedTopic("/asdf/asdfasfd/dfasf");
 
 	@Test
 	public void testPutGet() throws FceNoAuthorizationPossibleException {
 		UserConfiguration userConfig = new UserConfiguration(TESTUSER, TESTIDENTIFIER, ManagedPermission.EVERYONE, null,
 				null, null);
-		
-		AuthorizationProperties props = new AuthorizationProperties(null, null, TESTIDENTIFIER, null);
-		
-		ConfigurationDbService configService = new ConfigurationDbService(null);
-		configService.put(TOPIC_SAMPLE, userConfig);
 
-		assertTrue(configService
-				.getConfigurationForSingleManagedTopic(
-						ManagedZoneUtil.moveTopicIdentifierToZone(TOPIC_SAMPLE, ManagedZone.MANAGED_CONFIGURATION),props)
+		AuthorizationProperties props = new AuthorizationProperties(null, null, TESTIDENTIFIER, null);
+
+		ConfigurationDbService configService = new ConfigurationDbService(null);
+		configService.put(TOPIC_SAMPLE.getUserTopicIdentifier(props, ManagedZone.MANAGED_CONFIGURATION), userConfig);
+
+		assertTrue(configService.getConfigurationForSingleManagedTopic(TOPIC_SAMPLE, props)
 				.getUserIdentifier() == TESTIDENTIFIER);
-		
-		assertTrue(configService
-				.getConfigurationForSingleManagedTopic(
-						ManagedZoneUtil.moveTopicIdentifierToZone(TOPIC_SAMPLE_CONFIG_IN_SUBTOPIC, ManagedZone.MANAGED_CONFIGURATION), props)
+
+		assertTrue(configService.getConfigurationForSingleManagedTopic(TOPIC_SAMPLE_CONFIG_IN_SUBTOPIC, props)
 				.getUserIdentifier() == TESTIDENTIFIER);
 	}
 
@@ -44,10 +44,14 @@ public class ConfigurationDbServiceTest {
 	public void testPutGetException() throws FceNoAuthorizationPossibleException {
 		UserConfiguration userConfig = new UserConfiguration(TESTUSER, TESTIDENTIFIER, ManagedPermission.EVERYONE, null,
 				null, null);
-		ConfigurationDbService configService = new ConfigurationDbService(null);
-		configService.put(TOPIC_SAMPLE, userConfig);
+		
+		AuthorizationProperties props = new AuthorizationProperties(null, null, TESTIDENTIFIER, null);
 
-		assertTrue(configService.getConfigurationForSingleManagedTopic(TOPIC_INVALID)
+		ConfigurationDbService configService = new ConfigurationDbService(null);
+		configService.put(TOPIC_SAMPLE.getUserTopicIdentifier(props, ManagedZone.MANAGED_CONFIGURATION), userConfig);
+
+
+		assertTrue(configService.getConfigurationForSingleManagedTopic(TOPIC_INVALID, props)
 				.getUserIdentifier() == TESTIDENTIFIER);
 	}
 
@@ -55,11 +59,40 @@ public class ConfigurationDbServiceTest {
 	public void testIsManaged() throws FceNoAuthorizationPossibleException {
 		UserConfiguration userConfig = new UserConfiguration(TESTUSER, TESTIDENTIFIER, ManagedPermission.EVERYONE, null,
 				null, null);
+		AuthorizationProperties props = new AuthorizationProperties(null, null, TESTIDENTIFIER, null);
+
 		ConfigurationDbService configService = new ConfigurationDbService(null);
-		configService.put(TOPIC_SAMPLE, userConfig);
+		configService.put(TOPIC_SAMPLE.getUserTopicIdentifier(props, ManagedZone.MANAGED_CONFIGURATION), userConfig);
 
 		assertTrue(configService.isTopicFilterManaged(TOPIC_SAMPLE));
 		assertFalse(configService.isTopicFilterManaged(TOPIC_INVALID));
 	}
 
+	@Test
+	public void testPutGetEveryone() throws FceNoAuthorizationPossibleException {
+		UserConfiguration userConfig = new UserConfiguration(TESTUSER, TESTIDENTIFIER, ManagedPermission.EVERYONE, null,
+				null, null);
+		
+		UserConfiguration everyoneConfig = new UserConfiguration(ALLUSER, ALLIDENTIFIER, ManagedPermission.EVERYONE, null,
+				null, null);
+
+		AuthorizationProperties props = new AuthorizationProperties(null, null, TESTIDENTIFIER, null);
+
+		ConfigurationDbService configService = new ConfigurationDbService(null);
+		configService.put(TOPIC_SAMPLE.getEveryoneTopicIdentifier(ManagedZone.MANAGED_CONFIGURATION), everyoneConfig);
+
+		assertTrue(configService.getConfigurationForSingleManagedTopic(TOPIC_SAMPLE, props)
+				.getUserIdentifier() == ALLIDENTIFIER);
+
+		assertTrue(configService.getConfigurationForSingleManagedTopic(TOPIC_SAMPLE_CONFIG_IN_SUBTOPIC, props)
+				.getUserIdentifier() == ALLIDENTIFIER);
+		
+		configService.put(TOPIC_SAMPLE.getUserTopicIdentifier(props, ManagedZone.MANAGED_CONFIGURATION), userConfig);
+
+		assertTrue(configService.getConfigurationForSingleManagedTopic(TOPIC_SAMPLE, props)
+				.getUserIdentifier() == TESTIDENTIFIER);
+
+		assertTrue(configService.getConfigurationForSingleManagedTopic(TOPIC_SAMPLE_CONFIG_IN_SUBTOPIC, props)
+				.getUserIdentifier() == TESTIDENTIFIER);
+	}
 }
